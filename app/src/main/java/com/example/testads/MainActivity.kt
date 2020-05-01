c2af046852c98ea892e536ed5e9a24e78329f098
Dio.Pratama <dpratama.43@gmail.com>
2020-05-01 21:15:29 +0700

    Test Ads Banner & Interstitial

---
+++ app/src/main/java/com/example/testads/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.testads
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.MobileAds
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mInterstitialAd: InterstitialAd
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111")
+        adView.loadAd(AdRequest.Builder().build())
+
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+
+        mMyButton.setOnClickListener {
+            if (mInterstitialAd.isLoaded) {
+                mInterstitialAd.show()
+            } else {
+                Log.d("TAG", "The interstitial wasn't loaded yet.")
+            }
+
+
+        }
+    }
+}
